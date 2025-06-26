b3ecf91cf1c49192cceecb72d956501265820215
Dipak Paswan <dipakpaswan9635@gmail.com>
2025-06-26 15:13:14 +0530

    Implement comment management functionality: add, update, and delete comments with validation and user ownership checks

--- src/controllers/comments.controllers.js
+++ src/controllers/comments.controllers.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 import { Comments } from "../models/comments.model.js";
-import { video } from "../models/video.model.js";
+import { video, video } from "../models/video.model.js";
 import { Like } from "../models/likes.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -38,8 +38,131 @@ const getVideoComments = asyncHandler(async (req, res)=>{
         },
         {
             $addFields: {
-                
+                likesCount: {
+                    $size: "$likes"
+                },
+                owner: {
+                    $first: "$owner"
+                },
+                isLiked: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id,"$likes.likedBy"]
+                        },
+                        then: true,
+                        else: false,
+                    }
+                }
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1 // sort by creation date in descending order
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                likesCount: 1,
+                owner: {
+                    username: 1,
+                    fullname: 1,
+                    "avatar.url": 1,
+                },
+                isLiked: 1,
             }
         }
-    ])
-})
+    ]);
+    const options = {
+        page: parseInt(page),
+        limit: parseInt(limit),
+    };
+    const comments = await Comment.aggregatePaginate(
+        commentAggregate,
+        options
+    );
+    return res
+    .status(200)
+    .json(new ApiResponse(200, "comments fetched successfully"));
+});
+
+const addComment = asyncHandler(async (req,res)=>{
+    const {videoId} = req.params;
+    const {content} = req.body;
+
+    if(!content){
+        throw new ApiError(400, "comment content is required");
+    }
+    const video = await video.findById(videoId);
+    if(!video){
+        throw new ApiError(404, "video not found");
+    }
+    const comment = await comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id,
+    });
+
+    if(!comment){
+        throw new ApiError(500,"unable to add comment");
+    }
+    return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "comment added successfully"));
+});
+
+const updateComment = asyncHandler(async (req,res)=>{
+    const {commentId} = req.params;
+    const {content} = req.body;
+    if(!content){
+        throw new ApiError(400,"comment content is required to update");
+    }
+    const comment = await Comment.findById(commentId);
+    if(!comment){
+        throw new ApiError(404,"comment not found");
+    }
+    if(comment.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403,"You are not allowed to update this comment");
+    }
+
+    const updatedComment = await Comment.findByAndUpdate(comment?._id,
+        {
+            $set: {
+                content,
+            }
+        },
+        { new: true }
+    );
+    if(!updatedComment){
+        throw new ApiError(500,"Failed to edit comment please try again");
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updateComment, "comment updated successfully"));
+});
+
+// deleteComment
+const deleteComment = asyncHandler(async (req,res)=>{
+    const {commentId} = req.params;
+
+    const comment = await Comment.findById(commentId);
+    if(!comment){
+        throw new ApiError(404,"comment not found");
+    }
+    if(comment.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403,"You are not allowed to delete this comment");
+    }
+
+    await Comment.findByAndDelete(commentId);
+
+    await Like.deleteMany({
+        comment: commentId,
+        likedBy: req.user
+    });
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {commentId}, "comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updatedComment, deleteComment};
